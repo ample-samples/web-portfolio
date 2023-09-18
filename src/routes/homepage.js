2038c85c67acc8ea1805ad216c9b2a57c69d7a8f
Todd Griffin <todd.griffin61@outlook.com>
2023-09-18 15:55:13 +0100

    fixed weird interaction with menu opening on narrow displays

--- src/routes/homepage.js
+++ src/routes/homepage.js
@@ -21,14 +21,11 @@ export function Homepage() {
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
+            flexWrap: "wrap"
         }}>
           <Typography variant='h4'>Main Tech stack and Tools</Typography>
         </Box>
-          <Box className="tech-stack" sx={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: 'flex-start',
-          }}>
+        <div className="tech-stack">
               <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> </a>
               <a href="https://reactjs.org/" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40"/> </a> 
               <a href="https://expressjs.com" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg" alt="express" width="40" height="40"/> </a>
@@ -38,7 +35,7 @@ export function Homepage() {
               <a href="https://nodejs.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg" alt="nodejs" width="40" height="40"/> </a>
               <a href="https://www.postgresql.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg" alt="postgresql" width="40" height="40"/> </a>
               <a href="https://www.python.org" target="_blank" rel="noreferrer"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" alt="python" width="40" height="40"/> </a>
-          </Box>
+        </div>
       </Container>
     </>
   )
