80e3a434ea6486f2f828d01bec3e5c2f0dcffab6
Todd Griffin <todd.griffin61@outlook.com>
2023-11-18 22:32:27 +0000

    improved mobile display

--- src/routes/try-mobile.js
+++ src/routes/try-mobile.js
@@ -1,6 +1,8 @@
 import { Container, Typography, Box } from "@mui/material"
 import iPhoneShell from '../files/pictures/iPhone14_shell.png'
 import iPhoneFull from '../files/pictures/iPhone_chrome_full_ui.png'
+import device from '../files/pictures/device.svg'
+import deviceGlass from '../files/pictures/glass.svg'
 import './style.css'
 
 export function TryMobile() {
@@ -11,7 +13,10 @@ export function TryMobile() {
       <Container component="main" maxWidth="md">
         <div className="try-mobile">
           <Typography variant='h4' sx={{ padding: "30px" }}>A responsive layout is essential, here's this site's!</Typography>
-          <div className="center" style={{ position: "relative", top: 0, left: 0, height: 850 }}>
+          <div className="center" style={{ position: "relative", top: 0, left: 0, height: 870 }}>
+            <Box sx={{ position: "absolute", top: -7, pointerEvents: "none" }} >
+              <img height="866" src={device} alt="iPhone" style={{borderRadius: 23}} />
+            </Box>
             <Box sx={{ position: "relative", top: 104, left: 0 }}>
               <iframe
                 className="frame-area"
@@ -28,6 +33,9 @@ export function TryMobile() {
             </Box>
             <Box sx={{ position: "absolute", top: 0, pointerEvents: "none" }} >
               <img height="850" src={iPhoneShell} alt="iPhone" />
+            </Box>
+            <Box sx={{ position: "absolute", top: 2, pointerEvents: "none" }} >
+              <img height="850" src={deviceGlass} alt="iPhone" style={{borderRadius: 23}} />
             </Box>
 
           </div>
